c7132623e0820d32ac612f797a9afaf7e08b136d
Arnav-soni <73822785+Arnav-soni@users.noreply.github.com>
2021-02-01 12:49:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 var pics_array = [ "Familybookmage.jpg" ,"collage.png", "Arnav.jpg", "Karthik.jpg", "Richa.jpg", "Richa_arnav.jpg"];
 
 var text_array = ["Family collage", "Arnav", " Me and My Father", "My mother", "Me and My Mother", "Family Book"];
-var o = 1;
-var y = 0;
+var i = 1;
+var a = 0;
 
 function next() {
 
-  if (o == 6) {
-    o = 0;
+  if (i == 6) {
+    i = 0;
     document.getElementById("heading").style.display = "none";
   };
 
-  document.getElementById("image1").src = pics_array[o];
-  o++;
+  document.getElementById("image1").src = pics_array[i];
+  i++;
 
-  if (y == 6) {
-    y = 0;
+  if (a == 6) {
+    a = 0;
   };
 
-  document.getElementById("family_name").innerHTML = text_array[y];
-  y++;
+  document.getElementById("family_name").innerHTML = text_array[a];
+  a++;
 
 };
 
